8ffc88a67c165833fae3cd0d601b1ecf21fea0fa
CleanRoar <sosyalkenan8@gmail.com>
2024-11-07 15:48:36 +0400

    1

---
+++ src/praktika1.java
@@ -0,0 +1,52 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class praktika1 {
+    public static void main(String[] args){
+       Scanner sc = new Scanner (System.in);
+       var a =sc.next();
+       var b = "admin";
+       if(a.equals(b)) {
+           System.out.println(a +"Welcome");
+       }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
